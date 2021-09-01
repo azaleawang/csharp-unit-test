10714cea857101751cf833e7c4e8f86aa20a237d
vans.tsai81 <vans.tsai81@titansoft.com.sg>
2021-09-01 22:49:07 +0800

    modify

--- UnitTestCourse/TiPayRequestValidatorTests.cs
+++ UnitTestCourse/TiPayRequestValidatorTests.cs
@@ -8,6 +8,16 @@ namespace UnitTestCourse
     [TestFixture]
     public class TiPayRequestValidatorTests
     {
+        private IMerchantRepository _IMerchantRepository;
+        private TiPayRequestValidator _TiPayRequestValidator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _IMerchantRepository = Substitute.For<IMerchantRepository>();
+            _TiPayRequestValidator = new TiPayRequestValidator(_IMerchantRepository);
+        }
+
         // 5. write unit tests for TiPayRequestValidator
         [Test]
         public void Validate_WrongSignature_ShouldThrowException()
@@ -19,10 +29,10 @@ namespace UnitTestCourse
                 Amount = 999,
                 Signature = "123"
             };
-            var merchantRepository = Substitute.For<IMerchantRepository>();
-            merchantRepository.GetMerchantKey(default).Returns("asdf1234");
-            var validator = new TiPayRequestValidator(merchantRepository);
-            var ex = Assert.Catch<Exception>(() => validator.Validate(request));
+
+            _IMerchantRepository.GetMerchantKey(Arg.Any<string>()).Returns("asdf1234");
+
+            var ex = Assert.Catch<Exception>(() => _TiPayRequestValidator.Validate(request));
             StringAssert.Contains("Signature mismatch", ex.Message);
         }
 
@@ -37,12 +47,8 @@ namespace UnitTestCourse
                 Signature = "137bdcd797c899a19e8b6abf5a81a901"
             };
 
-            var merchantRepository = Substitute.For<IMerchantRepository>();
-            
-            var validator = new TiPayRequestValidator(merchantRepository);
-            merchantRepository.GetMerchantKey(default).Returns("asdf1234");
-
-            Assert.DoesNotThrow(()=>validator.Validate(request));
+            _IMerchantRepository.GetMerchantKey(Arg.Any<string>()).Returns("asdf1234");
+            Assert.DoesNotThrow(()=> _TiPayRequestValidator.Validate(request));
         }
 
     }
